3ac0a45c7244dd31fae1932642b2e2f2bc1e0bc0
Pablo <Pablo@129.13.9.56>
2018-11-23 13:36:00 +0100

    Initial commit

---
+++ gruppenuebung1-gruppe1/src/gruppe1/gruppenuebung1/CSVImporter.java
@@ -0,0 +1,5 @@
+package gruppe1.gruppenuebung1;
+
+public class CSVImporter {
+
+}
---
+++ gruppenuebung1-gruppe1/src/gruppe1/gruppenuebung1/Gruppenuebung1_Gruppe1.java
@@ -0,0 +1,11 @@
+package gruppe1.gruppenuebung1;
+
+
+public class Gruppenuebung1_Gruppe1 {
+	
+	public static void main(String[] args) {
+		EmbeddingRepository repo = null;
+		
+	}
+	
+}
